2d5389d2895133c7517f3c9a4328b467dec886fe
cassidysimmons <cassidy1231@yahoo.com>
2024-02-15 18:26:10 -0600

    done :)

--- static/js/app.js
+++ static/js/app.js
@@ -1,92 +1,114 @@
 // use the D3 library to read in sample.json from the url
 const url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json';
 
+// create a function to initialize the plots
+function init(){
+
+    let dropDown = d3.select('#selDataset');
+
+    d3.json(url).then(function(data) {
+
+        let sampleNames = data.names;
+
+        sampleNames.forEach(element =>{
+            dropDown.append("option").text(element).property("value",element);
+        });
+
+        let sampleOne = sampleNames[0];
+
+        metaData(sampleOne);
+        barGraph(sampleOne);
+        bubbleChart(sampleOne);
+
+    });
+};
+
 // Create a horizontal bar chart with a dropdown menu to display
 // the top 10 OTUs found in that individual
-
-
-function barGraph(samples){
+function barGraph(sample){
     d3.json(url).then(function(response){
+
         samples = response.samples;
 
-        let resultArray = samples.filter(sampleObj => sampleObj.id == response);
+        let resultArray = samples.filter(sampleObj => sampleObj.id == sample);
         let result = resultArray[0];
 
-        data = [{
-            x:result.sample_values.slice(0, 10).reverse(),
-            y: result.otu_labels,
+        let data = [{
+            x: result.sample_values.slice(0, 10).reverse(),
+            y: result.otu_labels.slice(0, 10).map(id => `OTU ${id}`).reverse(),
+            labels: result.otu_labels.slice(0, 10).reverse(),
             type: 'bar',
             orientation: 'h'
         }];
  
-     Plotly.newPlot('bar', data);
+        let layout = {
+            title: 'top ten OTUs'
+        };
+
+     Plotly.newPlot('bar', data, layout);
  });
  };
 
-barGraph();
-
-// d3.select('#bar').append(barGraph);
-
 // Create a bubble chart that displays each sample
-function bubbleChart(samples){
+function bubbleChart(sample){
     d3.json(url).then(function(response){
 
         samples = response.samples;
 
-        let resultArray = samples.filter(sampleObj => sampleObj.id == response);
+        let resultArray = samples.filter(sampleObj => sampleObj.id == sample);
         let result = resultArray[0];
 
-    data = [{
+    let data = [{
         x: result.otu_ids,
         y: result.sample_values,
         text: result.otu_labels,
+        mode: "markers",
         marker: {
-            color: [result.otu_ids],
-            size: [result.sample_values]
+            color: result.otu_ids,
+            size: result.sample_values,
+            colorscale: "Earth"
         }
-    
     }];
 
-    Plotly.newPlot('bubble', data);
+    let layout = {
+        title: 'bacteria per sample',
+        hovermode: 'closest',
+        xaxis: {title: "OTU ID"}
+    }
+
+    Plotly.newPlot('bubble', data, layout);
     });
 };
 
-bubbleChart();
-
-
 // Display the sample metadata, i.e., an individual's demographic information.
-function metaData(){
+function metaData(sample){
     d3.json(url).then(function(response){
 
         metadata = response.metadata;
 
-        let resultArray = metadata.filter(sampleObj => sampleObj.id == response);
+        let resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
         let result = resultArray[0];
 
-    data = [{
-        type: 'table',
-        values: result.map(data => data.metadata),
-    }];
+        d3.select('#sample-metadata').html("");
 
-    Plotly.newPlot('sample-metadata', data);
+        Object.entries(result).forEach(([key, value])=>{
+            
+            d3.select('#sample-metadata').append("h5").text(`${key}: ${value}`);
+
+        });
 });
 };
 
-metaData();
-
 // Update all the plots when a new sample is selected. 
 // Additionally, you are welcome to create any layout that you would like for your dashboard
-function updatePlotly(){
-    let dropDown = d3.select('#selDataset');
+function optionChanged(value){
 
-    let dataSet = dropDown.property('value');
+    console.log(value);
 
-    d3.json(url).then(function(data) {
-
-    let sampleNames = data.names;
-
-    for (let i=0; i < sampleNames.length; i++){
-
-    }
-    });
+    metaData(value);
+    barGraph(value);
+    bubbleChart(value);
 };
+
+// call the function to initialize the data
+init();
